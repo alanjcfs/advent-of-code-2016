eb52600d538ea2291286902394c9a3a05514b651
Alan Fung-Schwarz <alan.jcfs@gmail.com>
2016-12-08 22:14:13 -0800

    Fix mistakes, ensure the sum of points

--- day_1-rust/src/main.rs
+++ day_1-rust/src/main.rs
@@ -71,8 +71,6 @@ fn main() {
     let mut face = Direction::North;
     let mut point = Point{x: 0, y: 0};
 
-    face = turn(&Instruction{turn: Turn::L, steps: 10}, &face);
-
     let mut s = String::from("R3 L2 L2 R4 L1 R2 R3 R4 L2 R4 L2 L5 L1 R5 R2 R2 L1 R4 R1 L5 L3 R4 R3 R1 L1 \
        L5 L4 L2 R5 L3 L4 R3 R1 L3 R1 L3 R3 L4 R2 R5 L190 R2 L3 R47 R4 L3 R78 L1 R3 \
        R190 R4 L3 R4 R2 R5 R3 R4 R3 L1 L4 R3 L4 R1 L4 L5 R3 L3 L4 R1 R2 L4 L3 R3 R3 \
@@ -94,5 +92,5 @@ fn main() {
         }
     }
 
-    println!("{:?}", point);
+    println!("{:?}", point.x.abs() + point.y.abs());
 }
